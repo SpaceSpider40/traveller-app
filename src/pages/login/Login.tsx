0986883669833d64594a313d6a85d351e722f737
Oskar Ochal <spacespiderproductions@gmail.com>
2024-06-23 00:02:13 +0200

    tweaks to login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -5,9 +5,13 @@ import google_logo from "../../assets/img/google_logo.png";
 
 export default class Login extends Component {
     render() {
-        return <div className="grid grid-cols-1 md:grid-cols-3 h-screen">
-            <div className="min-h-full md:col-span-2"></div>
-            <div className="min-h-full justify-center content-center">
+        return <div className="grid grid-cols-1 md:grid-cols-3 h-screen bg-no-repeat bg-fixed bg-cover bg-[url('/src/assets/img/winter_mountains.jpg')] ">
+            <div className="min-h-full md:col-span-2">
+                <div className="min-h-full justify-center content-center">
+                </div>
+            </div>
+
+            <div className="min-h-full justify-center content-center bg-blue-700/30 backdrop-blur-md">
                 <div className={" p-10"}>
                     <p className={"text-7xl mx-2 my-10"}>Traveller</p>
 
